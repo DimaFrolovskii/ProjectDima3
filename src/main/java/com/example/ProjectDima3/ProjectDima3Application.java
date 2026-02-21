6223e8323af9326e6717b51f6301758c38482509
DimaFrolovskii <skuvi007@gmail.com>
2026-02-21 09:20:47 +0300

    init: Базовая структура Spring Boot с зависимостями Web, JPA, Security, Liquibase

---
+++ src/main/java/com/example/ProjectDima3/ProjectDima3Application.java
@@ -0,0 +1,13 @@
+package com.example.ProjectDima3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDima3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDima3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectDima3/ProjectDima3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectDima3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDima3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
